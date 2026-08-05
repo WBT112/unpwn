3c0d7393787227cc02c9c544bd413b93f238e4e3
Tobi <98946003+WBT112@users.noreply.github.com>
2026-08-05 19:49:14 +0200

    Add explicit decrypted vault record lifetime

--- src/Unpwn.Vault/Storage/VaultRecord.cs
+++ src/Unpwn.Vault/Storage/VaultRecord.cs
@@ -1,5 +1,31 @@
+using System.Security.Cryptography;
 using Unpwn.Vault.Cryptography;
 
 namespace Unpwn.Vault.Storage;
 
-public sealed record VaultRecord(VaultRecordDescriptor Descriptor, byte[] Plaintext);
+public sealed class VaultRecord : IDisposable
+{
+    private byte[]? _plaintext;
+
+    public VaultRecord(VaultRecordDescriptor descriptor, byte[] plaintext)
+    {
+        Descriptor = descriptor ?? throw new ArgumentNullException(nameof(descriptor));
+        _plaintext = plaintext ?? throw new ArgumentNullException(nameof(plaintext));
+    }
+
+    public VaultRecordDescriptor Descriptor { get; }
+
+    public ReadOnlyMemory<byte> Plaintext =>
+        _plaintext ?? throw new ObjectDisposedException(nameof(VaultRecord));
+
+    public void Dispose()
+    {
+        if (_plaintext is null)
+        {
+            return;
+        }
+
+        CryptographicOperations.ZeroMemory(_plaintext);
+        _plaintext = null;
+    }
+}
